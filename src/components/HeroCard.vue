1c2d06d3fcdebb0490ecf51df65039b736863314
starwen <hihehello00@gmail.com>
2026-08-10 23:05:44 +0800

    revert changes of HeroCard.vue

--- src/components/HeroCard.vue
+++ src/components/HeroCard.vue
@@ -35,8 +35,7 @@ function processCardType(type: Card['type']) {
 <template>
     <div class="hero-card-wrapper" :class="{ [`hero-card-color-${card.color}`]: true, 'has-faq': hasFaq }"
         @click="showFAQs()">
-        <div class="hero-card-name">{{ $t(card.name) }}</div>
-        <!--<div class="hero-card-initiative">{{ card.initiative }}</div>-->
+        <div class="hero-card-name">{{ card.name }}</div>
         <div v-if="card.tier" class="hero-card-tier">{{ card.tier }}</div>
         <div class="hero-card-type">
             {{ processCardType(card.type) }}
@@ -124,8 +123,8 @@ function processCardType(type: Card['type']) {
         min-width: 95%;
 
         position: relative;
-        font-family: 'ModestoPoster';
-        font-size: 1.5em;
+        font-family: 'ModestoPoster', serif;
+        font-size: 1.6em;
 
         color: var(--color-text-dark);
         text-shadow: 0 0 3px #FFFFFF;
